4769c46e1d5e6ec8cd9d4828f588d834ec883dcd
bboubacar <bboubacarfr@gmail.com>
2024-08-30 14:41:56 +0200

    Add env variables

--- Config/db.php
+++ Config/db.php
@@ -1,11 +1,22 @@
 <?php
 // Informations de connexion
 if ($_SERVER["SERVER_ADDR"] !== "127.0.0.1") {
-    define('SGBDR', 'mysql');
-    define('DBHOST', 'mysql-bcr.alwaysdata.net');
-    define('DBUSER', 'bcr');
-    define('DBPASS', 'Bouba-Web2022');
-    define('DBNAME', 'bcr_offres_emploi');
+    // define('SGBDR', 'mysql');
+    // define('DBHOST', 'mysql-bcr.alwaysdata.net');
+    // define('DBUSER', 'bcr');
+    // define('DBPASS', 'Bouba-Web2022');
+    // define('DBNAME', 'bcr_offres_emploi');
+    //     SGBDR = mysql
+    // SGBDR = mysql
+    // DBHOST = mysql-bcr.alwaysdata.net
+    // DBUSER = bcr
+    // DBPASS = Bouba-Web2022
+    // DBNAME = bcr_offres_emploi
+    define('SGBDR', $_ENV['SGBDR']);
+    define('DBHOST', $_ENV['DBHOST']);
+    define('DBUSER', $_ENV['DBUSER']);
+    define('DBPASS', $_ENV['DBPASS']);
+    define('DBNAME', $_ENV['DBNAME']);
 } else {
     define('SGBDR', 'mysql');
     define('DBHOST', 'localhost');
